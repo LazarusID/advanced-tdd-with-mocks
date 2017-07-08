5f1c3fd7f1147a6a18d32a7fdde23fbaa4ebc688
Clay Dowling <clay@lazarusid.com>
2017-07-08 10:37:28 -0400

    Passing test using mock

--- CartItem.cpp
+++ CartItem.cpp
@@ -2,7 +2,10 @@
 // Created by clay on 7/4/17.
 //
 
+#include <sstream>
 #include "CartItem.h"
+
+const int FIELD_UNSET = -1;
 
 CartItem::CartItem() : name(""), quantity(0), price(0.0)
 {}
@@ -40,5 +43,34 @@ double CartItem::getItemTotal() {
 }
 
 DataResult& operator>>(DataResult& in, CartItem& item) {
+
+    int name_idx = FIELD_UNSET;
+    int price_idx = FIELD_UNSET;
+    int quantity_idx = FIELD_UNSET;
+
+    auto fields = in.getFields();
+
+    for(int i=0; i < fields->size(); ++i) {
+        if ("name" == fields->at(i)) name_idx = i;
+        if ("price" == fields->at(i)) price_idx = i;
+        if ("quantity" == fields->at(i)) quantity_idx = i;
+    }
+
+    auto row_ptr = in.getNext();
+    auto row = *row_ptr;
+    item.setName(row[name_idx]);
+
+    istringstream inprice(row[price_idx]);
+    istringstream inquantity(row[quantity_idx]);
+
+    double price;
+    int quantity;
+
+    inprice >> price;
+    inquantity >> quantity;
+
+    item.setPrice(price);
+    item.setQuantity(quantity);
+
     return in;
 }
